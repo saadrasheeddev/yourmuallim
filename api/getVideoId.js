a84fd0a7439552d25ccaf401632670a1a8571a16
saadrasheeddev <84860156+saadrasheeddev@users.noreply.github.com>
2023-08-04 19:09:58 +0500

    Update getVideoId.js testing fallback

--- api/getVideoId.js
+++ api/getVideoId.js
@@ -3,8 +3,8 @@
 // This function will be responsible for fetching and returning the video ID
 const getVideoId = (req, res) => {
   // Replace this with your logic to fetch the video ID from your data source
-  const videoId = 'hrnT2IFqyro';
-  // const videoId = 'abc'; testing fallback (failed)
+  // const videoId = 'hrnT2IFqyro';
+  const videoId = null; // testing fallback (failed)
   res.json({ videoId });
 };
 
