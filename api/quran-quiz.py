3ecd2fe9d03a0f44e8327560cdef9a315d3449f5
Saad Rasheed <84860156+saadrasheeddev@users.noreply.github.com>
2024-01-15 15:34:38 +0500

    Update quran-quiz.py

--- api/quran-quiz.py
+++ api/quran-quiz.py
@@ -1,14 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
 import pandas as pd
 import random
 
-df = pd.read_csv("ayah_list.csv")
+app = Flask(__name__)
+CORS(app)
 
-ayah_list = list(df)
+@app.route('/get_random_ayah', methods=['GET'])
+def get_random_ayah():
+    df = pd.read_csv("ayah_list.csv")
+    ayah_list = list(df)
+    random_number = random.randint(0, len(ayah_list) - 1)
+    selected_ayah = ayah_list[random_number]
+    return jsonify({"selected_ayah": selected_ayah.split(" ")})
 
-random_number = random.randint(0, len(ayah_list) - 1)
-
-selected_ayah = ayah_list[random_number]
-
-print(selected_ayah.split(" "))
-
-print(f"Randomly selected ayah: {selected_ayah}")
+if __name__ == '__main__':
+    app.run(debug=True)
