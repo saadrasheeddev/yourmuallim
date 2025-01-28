4031005749089f52edc72c8ec009d9fed75bdde3
Saad Rasheed <84860156+saadrasheeddev@users.noreply.github.com>
2025-01-28 18:14:01 +0500

    Update getVideoId.js

--- api/getVideoId.js
+++ api/getVideoId.js
@@ -3,8 +3,8 @@
 // This function will be responsible for fetching and returning the video ID
 const getVideoId = (req, res) => {
   // Replace this with your logic to fetch the video ID from your data source
-  const videoId = 'BXx8rzklFBY'; // Not working in Saudia
-  // const videoId = '7oMuSxy2mls';
+  // const videoId = 'BXx8rzklFBY'; // Not working in Saudia
+  const videoId = 'K5xvuvRQkBs';
   // const videoId = null; // testing fallback (pass)
   res.json({ videoId });
 };
